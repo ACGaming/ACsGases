847a49148fcac9045ae33e2528d5f46c73f5bcf7
ACGaming <4818419+ACGaming@users.noreply.github.com>
2026-03-01 11:08:10 +0100

    Enable finine config setting

--- src/main/java/net/trentv/gases/common/reaction/EntityReactionFinine.java
+++ src/main/java/net/trentv/gases/common/reaction/EntityReactionFinine.java
@@ -5,6 +5,8 @@ import java.util.Random;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+
+import net.trentv.gases.common.configuration.GasesMainConfigurations;
 import net.trentv.gasesframework.api.GasType;
 import net.trentv.gasesframework.api.reaction.entity.IEntityReaction;
 
@@ -16,7 +18,7 @@ public class EntityReactionFinine implements IEntityReaction
 	public void react(Entity e, IBlockAccess access, GasType gas, BlockPos pos)
 	{
 		BlockPos originalPosition = e.getPosition();
-		BlockPos newPosition = new BlockPos(originalPosition);
+		BlockPos newPosition;
 		int iterations = 0;
 		do
 		{
@@ -28,7 +30,7 @@ public class EntityReactionFinine implements IEntityReaction
 			int newZ = random.nextInt(16) - 8;
 			newPosition = newPosition.add(newX, newY, newZ);
 
-		} while (iterations < 200 & (access.isAirBlock(pos) & access.isAirBlock(pos.up())));
+		} while (iterations < GasesMainConfigurations.GASES.FININE.maxTeleportSearches & (access.isAirBlock(pos) & access.isAirBlock(pos.up())));
 
 		e.setPositionAndRotation(newPosition.getX(), newPosition.getY(), newPosition.getZ(), random.nextInt(360), random.nextInt(180) - 90);
 	}
